4cb1da1bd4ed402c3dee33c8f794e50f24a553e0
Pachika22 <89666337+Pachika22@users.noreply.github.com>
2022-03-29 13:31:41 -0400

    sub and mul addded

--- Example1.py
+++ Example1.py
@@ -1,4 +1,7 @@
 a = 10
 b=20
 print(a)
-print("Addition of a and b is:"+a+b)
+print("Addition of a and b is:",a+b)
+
+print("Substraction ob b and a is:",b-a)
+print("Multiplication of these is:",a*b)
